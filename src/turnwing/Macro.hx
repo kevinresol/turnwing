7d0d2e88260eb6044b2ec82ce38beb56571eb39a
Kevin Leung <kevinresol@gmail.com>
2017-08-12 00:51:22 +0800

    Factorization

--- src/turnwing/Macro.hx
+++ src/turnwing/Macro.hx
@@ -56,69 +56,67 @@ class Macro {
 		});
 	}
 	
+	static function getInterface(type:Type):ClassType
+		return switch type {
+			case TInst(_.get() => cls, _) if(cls.isInterface):
+				cls;
+			default:
+				throw type.getID() + ' show be an interface';
+		}
 	
 	static function getDataFields(type:Type):Array<Field> {
-		return switch type {
-			case TInst(_.get() => cls, _) if(cls.isInterface):
-				var fields:Array<Field> = [];
-				
-				for(field in cls.fields.get())
-					fields.push({
-						name: field.name,
-						kind: FVar(macro:String, null),
-						pos: field.pos
-					});
-					
-				fields;
-				
-			default:
-				throw type.getID() + ' show be an interface';
-		}
+		var cls = getInterface(type);
+		var fields:Array<Field> = [];
+		
+		for(field in cls.fields.get())
+			fields.push({
+				name: field.name,
+				kind: FVar(macro:String, null),
+				pos: field.pos
+			});
+			
+		return fields;
 	}
 	
-	static function getLocaleFields(type:Type):Array<Field>
-		return switch type {
-			case TInst(_.get() => cls, _) if(cls.isInterface):
-				var fields:Array<Field> = [];
-				
-				for(field in cls.fields.get())
-					switch field.type {
-						case TFun(a, ret):
-							
-							var name = field.name;
-							var args:Array<FunctionArg> = [];
-							var params = [];
-							
-							for(arg in a) {
-								args.push({
-									name: arg.name,
-									opt: arg.opt,
-									type: arg.t.toComplex(),
-								});
-								params.push({
-									field: arg.name,
-									expr: macro $i{arg.name},
-								});
-							}
-						
-							fields.push({
-								access: [APublic],
-								name: name,
-								kind: FFun({
-									args: args,
-									ret: macro:String,
-									expr: macro return __template__.execute(__data__.$name, ${EObjectDecl(params).at(field.pos)}),
-								}),
-								pos: field.pos,
-							});
-							
-						default:
-							field.pos.error('Locale interface can only define functions');
-					}
+	static function getLocaleFields(type:Type):Array<Field> {
+		var cls = getInterface(type);
+		var fields:Array<Field> = [];
+		
+		for(field in cls.fields.get())
+			switch field.type {
+				case TFun(a, ret):
 					
-				fields;
+					var name = field.name;
+					var args:Array<FunctionArg> = [];
+					var params = [];
+					
+					for(arg in a) {
+						args.push({
+							name: arg.name,
+							opt: arg.opt,
+							type: arg.t.toComplex(),
+						});
+						params.push({
+							field: arg.name,
+							expr: macro $i{arg.name},
+						});
+					}
 				
-			default:
-				throw type.getID() + ' show be an interface';
-		}
+					fields.push({
+						access: [APublic],
+						name: name,
+						kind: FFun({
+							args: args,
+							ret: macro:String,
+							expr: macro return __template__.execute(__data__.$name, ${EObjectDecl(params).at(field.pos)}),
+						}),
+						pos: field.pos,
+					});
+					
+				default:
+					field.pos.error('Locale interface can only define functions');
+			}
+			
+		return fields;
+	}
 }
