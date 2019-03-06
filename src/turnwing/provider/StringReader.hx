ef3eba47e7d9267beecd06a3d783aec8ee8f2712
Kevin Leung <kevinresol@gmail.com>
2019-03-06 15:38:06 +0800

    Add the missing @:from metadata

--- src/turnwing/provider/StringReader.hx
+++ src/turnwing/provider/StringReader.hx
@@ -5,12 +5,15 @@ using tink.CoreApi;
 @:forward
 abstract StringReader(StringReaderObject) from StringReaderObject to StringReaderObject {
 	
+	@:from
 	public static inline function ofSyncFunction(f:Language->String):StringReader
 		return ofPromiseFunction(function(lang):Promise<String> return f(lang));
 	
+	@:from
 	public static inline function ofFutureFunction(f:Language->Future<String>):StringReader
 		return ofPromiseFunction(function(lang):Promise<String> return f(lang));
 	
+	@:from
 	public static inline function ofPromiseFunction(f:Language->Promise<String>):StringReader
 		return new SimpleStringReader(f);
 		
