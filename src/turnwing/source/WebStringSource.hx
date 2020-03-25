6112ab62f2c3512b8862c25c8c0f1eaf481864e9
Kevin Leung <kevinresol@gmail.com>
2020-03-25 13:33:39 +0800

    Add a web source

---
+++ src/turnwing/source/WebStringSource.hx
@@ -0,0 +1,13 @@
+package turnwing.source;
+
+import turnwing.source.Source;
+
+class WebStringSource implements Source<String> {
+	var getUrl:(lang:String) -> String;
+
+	public function new(getUrl)
+		this.getUrl = getUrl;
+
+	public function fetch(language:String):Promise<String>
+		return tink.http.Fetch.fetch(getUrl(language)).all().next(res -> res.body.toString());
+}
