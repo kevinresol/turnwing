75ed903f1b2e92bbef1b5e6c778941b0b7aab74e
Kevin Leung <kevinresol@gmail.com>
2021-05-20 12:59:15 +0800

    Cleanup

--- tests/DummyTest.hx
+++ tests/DummyTest.hx
@@ -21,7 +21,7 @@ class DummyTest {
 
 	public function child() {
 		final loc = new Dummy<ParentLocale>();
-		
+
 		function test(loc:MyLocale) {
 			asserts.assert(loc.empty() == '<empty>');
 			asserts.assert(loc.hello('World') == '<hello name:World>');
@@ -33,17 +33,4 @@ class DummyTest {
 		test(loc.const);
 		return asserts.done();
 	}
-
-	// public function extended() {
-	// 	final source = new ResourceStringSource(lang -> 'extended-$lang.json');
-	// 	final loc = new Manager<ExtendedLocale>(new JsonProvider<ExtendedLocale>(source, template));
-	// 	return loc.get('en').next(locale -> {
-	// 		asserts.assert(locale.extended() == 'Extension!');
-	// 		asserts.assert(locale.empty() == 'Hello, World!');
-	// 		asserts.assert(locale.hello('World') == 'Hello, World!');
-	// 		asserts.assert(locale.bool(true) == 'Yes');
-	// 		asserts.assert(locale.bool(false) == 'No');
-	// 		asserts.done();
-	// 	});
-	// }
 }
